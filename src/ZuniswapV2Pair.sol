98716c841227ad1fded3e50efee24ee7303115b9
Pratyush Singh Chauhan <109815356+Prog-block@users.noreply.github.com>
2023-06-23 11:45:18 +0530

    Update ZuniswapV2Pair.sol

--- src/ZuniswapV2Pair.sol
+++ src/ZuniswapV2Pair.sol
@@ -125,56 +125,6 @@ contract ZuniswapV2Pair is ERC20, Math {
         emit Burn(msg.sender, amount0, amount1, to);
     }
 
-    function swap(
-        uint256 amount0Out,
-        uint256 amount1Out,
-        address to,
-        bytes calldata data
-    ) public nonReentrant {
-        if (amount0Out == 0 && amount1Out == 0)
-            revert InsufficientOutputAmount();
-
-        (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
-
-        if (amount0Out > reserve0_ || amount1Out > reserve1_)
-            revert InsufficientLiquidity();
-
-        if (amount0Out > 0) _safeTransfer(token0, to, amount0Out);
-        if (amount1Out > 0) _safeTransfer(token1, to, amount1Out);
-        if (data.length > 0)
-            IZuniswapV2Callee(to).zuniswapV2Call(
-                msg.sender,
-                amount0Out,
-                amount1Out,
-                data
-            );
-
-        uint256 balance0 = IERC20(token0).balanceOf(address(this));
-        uint256 balance1 = IERC20(token1).balanceOf(address(this));
-
-        uint256 amount0In = balance0 > reserve0 - amount0Out
-            ? balance0 - (reserve0 - amount0Out)
-            : 0;
-        uint256 amount1In = balance1 > reserve1 - amount1Out
-            ? balance1 - (reserve1 - amount1Out)
-            : 0;
-
-        if (amount0In == 0 && amount1In == 0) revert InsufficientInputAmount();
-
-        // Adjusted = balance before swap - swap fee; fee stays in the contract
-        uint256 balance0Adjusted = (balance0 * 1000) - (amount0In * 3);
-        uint256 balance1Adjusted = (balance1 * 1000) - (amount1In * 3);
-
-        if (
-            balance0Adjusted * balance1Adjusted <
-            uint256(reserve0_) * uint256(reserve1_) * (1000 ** 2)
-        ) revert InvalidK();
-
-        _update(balance0, balance1, reserve0_, reserve1_);
-
-        emit Swap(msg.sender, amount0Out, amount1Out, to);
-    }
-
     function sync() public {
         (uint112 reserve0_, uint112 reserve1_, ) = getReserves();
         _update(
